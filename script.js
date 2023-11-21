b5292c2a5be82550bfb72a104093c11ebf5ea0a1
Devyn <e255043@edu.sbl.ch>
2023-11-21 16:07:46 +0100

    Did the first part of exercise 1

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-export function exercise01(args) {
-  const input = args
-  const result = []
 
+export function exercise01(args) {
+const input = args
+const result = []
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
+  if (currentElement === "e" && currentElement !="E") {
+    result.push("")
+  } else { 
     result.push(currentElement)
   }
+}
   return result.join("")
+  
 }
