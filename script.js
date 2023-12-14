83cb1110e406b7a89c2084a67fd08117e4ddd468
Devyn <e255043@edu.sbl.ch>
2023-12-14 14:29:55 +0100

    Inowfinishedexercise5becausei couldntdoexercise4

--- script.js
+++ script.js
@@ -1,40 +1,70 @@
-
 export function exercise01(args) {
-const input = args
-const result = []
+  const input = args
+  const result = []
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-  if (currentElement === "e") {
-    result.push("")
-  } else if (currentElement === "E") {
-    result.push("")
-  } else { 
-    result.push(currentElement)
+    if (currentElement === "e") {
+      result.push("")
+    } else if (currentElement === "E") {
+      result.push("")
+    } else {
+      result.push(currentElement)
+    }
   }
-}
   return result.join("")
-  
 }
 
 export function exercise02(args) {
-  return args.toUpperCase ()
+  return args.toUpperCase()
 }
+
 export function exercise03(args) {
   const input = args
   const result = []
   let count = 0
- 
+
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    //Should only include 'e' 
-if (currentElement === 'e'){
-  count = count + 1
-}
- 
-// Should only count 'E' 
-else if (currentElement === 'E'){
-  count = count + 1
-}
+    //Should only include 'e'
+    if (currentElement === "e") {
+      count = count + 1
+    }
+
+    // Should only count 'E'
+    else if (currentElement === "E") {
+      count = count + 1
+    }
   }
   return count
+}
+
+export function exercise04(args) {
+  const input = args
+  let count = 1
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+    // Check if the current character is a space
+    if (currentElement === " ") {
+      count = count + 1
+    }
+  }
+  return count
+}
+export function exercise05(args) {
+  const input = args
+  const result = []
+  let capitalLetters = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+
+    const asciiCode = currentElement.charCodeAt(0)
+    if (asciiCode >= 65 && asciiCode <= 90) {
+      return true
+    }
+  }
+
+  return false
 }
